954cc7b2a08bf71dff08f992dc38aba789585a06
Andrii “Hrytsiuk <andrii.hrytsiuk@gmail.com>
2026-01-19 12:42:09 +0100

    init

--- .github/workflows/scripts/main.ts
+++ .github/workflows/scripts/main.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+
 interface Message {
   text: string;
 }
@@ -6,4 +8,8 @@ export async function main() {
   const message: Message = { text: "Hello, World! This is the main TypeScript script." };
 
   console.log(JSON.stringify(message));
+
+  let content = fs.readFileSync(`${process.env.GITHUB_WORKSPACE}/message.txt`, 'utf-8');
+  console.log(`Content of message.txt: ${content}`);
+
 }
