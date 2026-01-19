1ae18b1aa4cefab5a69b4fba7253b9fb70e6105a
Andrii “Hrytsiuk <andrii.hrytsiuk@gmail.com>
2026-01-19 12:32:01 +0100

    init

--- .github/workflows/scripts/main.ts
+++ .github/workflows/scripts/main.ts
@@ -2,10 +2,8 @@ interface Message {
   text: string;
 }
 
-async function run() {
+export default async ()=> {
   const message: Message = { text: "Hello, World! This is the main TypeScript script." };
 
   console.log(JSON.stringify(message));
 }
-
-export = run;
